335a9ce1844cd28ce7cd404881f129bca8f80829
Pawel Muody Kochanek <pawel.kochanek@gmail.com>
2024-07-02 06:42:47 +0200

    chore: fix layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <h1>My Minis</h1>
-<div class="flex flex-wrap gap-4">
+<div class="flex flex-wrap justify-around sm:justify-between w-full gap-4">
 	{#each data as { id, name, url, creator, previewUrl }}
-		<div class="w-1/6 border-2 border-sky-400 p-2 min-h-24">
+		<div class="w-full sm:w-64 border-2 border-sky-400 p-2">
 			<a href={`https://www.myminifactory.com/object/${url}`} class="flex flex-col"
 				><img src={previewUrl} alt={name} />
 				<h2 class="font-bold">{name}</h2></a
